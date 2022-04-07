e13d301cacbbf4cabdceb22f70cf15265044c5b3
petterug <pettergroettheim@gmail.com>
2022-04-07 12:58:36 +0200

    Finished unicafe in part 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
 
-function App() {
+// Statistics
+const Statistics = ({good, neutral, bad}) => {
+  let allVotes = good + neutral + bad
+  let average = (good - bad) / allVotes
+  let positive = `${(good / allVotes) * 100}%`
+
+  if( allVotes === 0) {
+    return (
+      <>
+      <h1>statistics</h1>
+      <div>
+        <p>No feedback given</p>
+      </div>
+      </>
+    )
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <h1>statistics</h1>
+    <div>
+      <table>
+        <tbody>
+          <tr><StatisticLine text="good" value={good} /></tr>
+          <tr><StatisticLine text="neutral" value={neutral} /></tr>
+          <tr><StatisticLine text="bad" value={bad} /></tr>
+          <tr><StatisticLine text="all" value={allVotes} /></tr>
+          <tr><StatisticLine text="average" value={average} /></tr>
+          <tr><StatisticLine text="positive" value={positive} /></tr>
+        </tbody>
+      </table>
     </div>
+    </>
+  )
+
+}
+const StatisticLine = (props) => {
+
+  return(
+    <>
+    <td>
+      {props.text} 
+    </td>
+    <td>
+      {props.value}
+    </td>
+    </>
+  )
+}
+
+// Feedback
+const Feedback = (props) => {
+
+  return (
+    <>
+    <h1>give feedback</h1>
+    <div>
+      <Button onClick={ props.increaseGood } text={'Good'} ></Button>
+      <Button onClick={ props.increaseNeutral } text={'Neutral'} ></Button>
+      <Button onClick={ props.increaseBad } text={'Bad'} ></Button>
+    </div>
+    </>
+  )
+}
+
+//Button
+const  Button = ({onClick, text }) => {
+  return (
+    <>
+    <button onClick={onClick}>{text}</button>
+    </>
+  )
+}
+
+
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const increaseGood = () => setGood(good + 1)
+  const increaseNeutral = () => setNeutral(neutral + 1)
+  const increaseBad = () => setBad(bad + 1)
+
+  return (
+    <>
+      <Feedback increaseGood={increaseGood} increaseNeutral={increaseNeutral} increaseBad={increaseBad} />
+
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </>
   );
 }
 
