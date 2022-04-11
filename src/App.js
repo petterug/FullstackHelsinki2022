4eef5b99098981b88e8938a1a6d23ee64f771557
petterug <pettergroettheim@gmail.com>
2022-04-11 14:39:14 +0200

    Finished exercise 2.6

--- src/App.js
+++ src/App.js
@@ -1,60 +1,53 @@
-import Course from './components/Course'
+import { useState } from 'react'
 
+const People = ({ names }) => {
+  console.log(names);
+  return (
+    <div>
+      {names.map(name => <div key={name.name} > {name.name} </div>)}
+    </div>
+  )
+}
 
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('enter name..')
 
-const App = () => { 
-    const courses = [
-    {
-      name: 'Half Stack application development',
-      id: 1,
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2
-        },
-        {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3
-        },
-        {
-          name: 'Redux',
-          exercises: 11,
-          id: 4
-        }
-      ]
-    }, 
-    {
-      name: 'Node.js',
-      id: 2,
-      parts: [
-        {
-          name: 'Routing',
-          exercises: 3,
-          id: 1
-        },
-        {
-          name: 'Middlewares',
-          exercises: 7,
-          id: 2
-        }
-      ]
+  const addName = (event) => {
+    event.preventDefault()
+    console.log('button clicked', event.target)
+
+    const newPerson = {
+      name: newName
     }
-  ]
+    
+    setPersons(persons.concat(newPerson))
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+    
+    setNewName(event.target.value)
+
+  }
 
   return (
     <div>
-      {courses.map(course => 
-        <Course key={course.id} course={course} />
-      )}
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <People names={persons} />
     </div>
   )
-} 
+}
 
 export default App
