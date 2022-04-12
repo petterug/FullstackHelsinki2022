c44976130558ee72bca743640858923d541b4add
petterug <pettergroettheim@gmail.com>
2022-04-12 10:20:02 +0200

    finished exercise 2.9

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import { useState } from 'react'
 
-const People = ({ names }) => {
-  console.log(names);
+const People = ({ names, searchFilter }) => {
+
+  const peopleToShow = searchFilter ? names.filter(name => name.name.toLowerCase().includes(searchFilter.toLowerCase())) : names
+
   return (
     <div>
-      {names.map(name => <div key={name.name} > {name.name} {name.number} </div>)}
+      <h2>Numbers</h2>
+      {peopleToShow.map(name => <div key={name.name} > {name.name} {name.number} </div>)}
     </div>
   )
 }
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '92391563' }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('enter name..')
   const [newNumber, setNewNumber] = useState('enter number..')
+  const [searchFilter, setSearchFilter] = useState('')
 
   const addName = (event) => {
     event.preventDefault()
@@ -37,20 +44,25 @@ const App = () => {
     setNewNumber('')
   }
 
-  const handleNameChange = (event) => {
-    
+  const handleNameChange = (event) => {    
     setNewName(event.target.value)
-
   }
   const handleNumberChange = (event) => {
-    
     setNewNumber(event.target.value)
-
+  }
+  const handleSearchChange = (event) => {
+    setSearchFilter(event.target.value)
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with:
+        <input value={searchFilter} onChange={handleSearchChange} />
+      </div>
+      
+      <h2>Add new person</h2>
       <form onSubmit={addName}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -62,8 +74,8 @@ const App = () => {
           <button type="submit">add</button>
         </div>
       </form>
-      <h2>Numbers</h2>
-      <People names={persons} />
+      
+      <People names={persons} searchFilter={searchFilter} />
     </div>
   )
 }
