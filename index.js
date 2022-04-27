214a9a7bbe7923c68ae3c017470f889d6dd219a0
petterug <pettergroettheim@gmail.com>
2022-04-27 10:39:00 +0200

    finished exercise 3.2

--- index.js
+++ index.js
@@ -21,8 +21,15 @@ let persons = [
       "id": 4,
       "name": "Mary Poppendieck", 
       "number": "39-23-6423122"
-    }
+    },
+    { 
+        "id": 5,
+        "name": "IBM Watson", 
+        "number": "01-10-1010101011"
+      }
 ]
+
+const dateTime = new Date(Date.now())
 
 app.get('/', (request, response) => {
     response.send('<h1>Landing page lol</h1>')
@@ -32,6 +39,17 @@ app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
 
+app.get('/info', (request, response) => {
+    response.send(
+        `<div>
+            Phonebook has info for ${persons.length} people
+        </div>
+        <div>
+            ${dateTime}
+        </div>
+        `
+    )
+})
 
 const PORT = 3001
 app.listen(PORT, () => {
