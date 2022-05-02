e18ca77a0aa96253de555737059ed32a56573ddb
petterug <pettergroettheim@gmail.com>
2022-05-02 11:21:02 +0200

    aa

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 const app = express()
 
 app.use(express.json())
+app.use(express.static('build'))
 app.use(cors())
 
 morgan.token('body', (request, response) => {return JSON.stringify(request.body)})
