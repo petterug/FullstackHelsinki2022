73cce8a692bf5bab45f9893344ec3a154e1469f1
petterug <pettergroettheim@gmail.com>
2022-05-02 10:50:24 +0200

    edit

--- index.js
+++ index.js
@@ -14,38 +14,38 @@ app.use(morgan(':method :url :response-time :body'))
 
 
 let persons = [
-    { 
+    {
       "id": 1,
-      "name": "Arto Hellas", 
+      "name": "Arto Hellas",
       "number": "040-123456"
     },
-    { 
+    {
       "id": 2,
-      "name": "Ada Lovelace", 
+      "name": "Ada Lovelace",
       "number": "39-44-5323523"
     },
-    { 
+    {
       "id": 3,
-      "name": "Dan Abramov", 
+      "name": "Dan Abramov",
       "number": "12-43-234345"
     },
-    { 
+    {
       "id": 4,
-      "name": "Mary Poppendieck", 
+      "name": "Mary Poppendieck",
       "number": "39-23-6423122"
     },
-    { 
+    {
         "id": 5,
-        "name": "IBM Watson", 
+        "name": "IBM Watson",
         "number": "01-10-1010101011"
       }
 ]
 
 
 
-app.get('/', (request, response) => {
-    response.send('<h1>Landing page lol</h1>')
-})
+// app.get('/', (request, response) => {
+//     response.send('<h1>Landing page lol</h1>')
+// })
 
 app.get('/api/persons', (request, response) => {
     response.json(persons)
@@ -81,9 +81,9 @@ app.post('/api/persons', (request, response) => {
     if(!person.number)
         return response.status(402).json({ error: 'missing number' })
 
-    if(persons.find(el => el.name === person.name)) 
+    if(persons.find(el => el.name === person.name))
         return response.status(403).json({ error:"name must be unique"})
-    
+
     person.id = Math.floor(Math.random() * 10000)
     persons = persons.concat(person)
 
@@ -93,7 +93,7 @@ app.post('/api/persons', (request, response) => {
 
 app.get('/info', (request, response) => {
     const dateTime = new Date(Date.now())
-    
+
     response.send(
         `<div>
             Phonebook has info for ${persons.length} people
