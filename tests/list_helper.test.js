57cebcbd8fe193c85c9ec8e3942140ee28a271b8
petterug <pettergroettheim@gmail.com>
2022-07-26 11:40:16 +0200

    4.4 tests

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -6,3 +6,108 @@ test('dummy returns one', () => {
     const result = listHelper.dummy(blogs)
     expect(result).toBe(1)
 })
+
+describe('sums likes of all blogs', () => {
+    const emptyArray = []
+
+    const listWithOneBlog = [
+        {
+          _id: '5a422aa71b54a676234d17f8',
+          title: 'Go To Statement Considered Harmful',
+          author: 'Edsger W. Dijkstra',
+          url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+          likes: 5,
+          __v: 0
+        }
+      ]
+
+      const listWithTwoBlogs = [
+        {
+          _id: '5a422aa71b54a676234d17f8',
+          title: 'Go To Statement Considered Harmful',
+          author: 'Edsger W. Dijkstra',
+          url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+          likes: 5,
+          __v: 0
+        },
+        {
+            _id: '5a422aa71b5412g86h1h63a4',
+            title: 'My life as a eunuch',
+            author: 'Donald Trump',
+            url: 'http://www.shitlord.com/html',
+            likes: 1,
+            __v: 0
+          }
+      ]
+
+      const blogs = [
+        {
+          _id: "5a422a851b54a676234d17f7",
+          title: "React patterns",
+          author: "Michael Chan",
+          url: "https://reactpatterns.com/",
+          likes: 7,
+          __v: 0
+        },
+        {
+          _id: "5a422aa71b54a676234d17f8",
+          title: "Go To Statement Considered Harmful",
+          author: "Edsger W. Dijkstra",
+          url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+          likes: 5,
+          __v: 0
+        },
+        {
+          _id: "5a422b3a1b54a676234d17f9",
+          title: "Canonical string reduction",
+          author: "Edsger W. Dijkstra",
+          url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+          likes: 12,
+          __v: 0
+        },
+        {
+          _id: "5a422b891b54a676234d17fa",
+          title: "First class tests",
+          author: "Robert C. Martin",
+          url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+          likes: 10,
+          __v: 0
+        },
+        {
+          _id: "5a422ba71b54a676234d17fb",
+          title: "TDD harms architecture",
+          author: "Robert C. Martin",
+          url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+          likes: 0,
+          __v: 0
+        },
+        {
+          _id: "5a422bc61b54a676234d17fc",
+          title: "Type wars",
+          author: "Robert C. Martin",
+          url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+          likes: 2,
+          __v: 0
+        }  
+      ]
+
+      test('likes when only one blog equals likes for that blog', () => {
+        const result = listHelper.totalLikes(listWithOneBlog)
+        expect(result).toBe(5)
+      })
+
+      test('likes for more than 1 blog', () => {
+        const result = listHelper.totalLikes(listWithTwoBlogs)
+        expect(result).toBe(6)
+      })
+
+      test('likes for many blogs', () => {
+        const result = listHelper.totalLikes(blogs)
+        expect(result).toBe(36)
+      })
+
+      test('empty array', () => {
+        const result = listHelper.totalLikes(emptyArray)
+        expect(result).toBe('empty list')
+      })
+})
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,14 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    return blogs.length === 0
+    ? 'empty list'
+    : blogs.reduce((accumulator, blog) => accumulator + blog.likes, 0)
+}
+
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
